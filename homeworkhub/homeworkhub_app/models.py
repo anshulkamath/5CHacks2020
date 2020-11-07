ba920800db5cd2f35ad6a4addb3d4eb14d184cb8
Marcos Acosta <macosta1625@gmail.com>
2020-11-07 15:58:08 -0500

    Created bat signal

--- homeworkhub/homeworkhub_app/models.py
+++ homeworkhub/homeworkhub_app/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 
 class Course(models.Model):
   name = models.CharField(max_length=100)
-  students = models.ManyToManyField(User)
+  students = models.ManyToManyField(User, through="BatSignal")
 
   def __str__(self):
     return self.name
+
+class BatSignal(models.Model):
+  user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+  course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+  active = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.active
--- homeworkhub/users/views.py
+++ homeworkhub/users/views.py
@@ -8,8 +8,8 @@ def register(request):
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
-      messages.success(request, 'Account created for {}'.format(username))
-      return redirect('homeworkhub_home')
+      messages.success(request, 'Your account has been created! Please log in to continue.')
+      return redirect('login')
   else:
     form = UserRegisterForm()
   return render(request, 'users/register.html', {'form': form})
