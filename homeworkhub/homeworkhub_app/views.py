aeea26ac0fdc45fc71de49e5ceef2509950d15d2
Marcos Acosta <macosta1625@gmail.com>
2020-11-07 14:11:56 -0500

    Added users and a nice gradient

--- homeworkhub/homeworkhub_app/views.py
+++ homeworkhub/homeworkhub_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Course
 
 def home(request):
   context = {
     "pagetitle": "hwh",
+    "courses": Course.objects.all()
   }
   return render(request, 'homeworkhub_app/home.html', context)
